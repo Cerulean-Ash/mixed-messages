4fbce0a9bb3dc8bf980b8d0b896df4dbb3fcd5c2
Ashley Lampard <sesshiru@gmail.com>
2021-12-29 06:30:35 +0000

    added message object with arrays of exercises to script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 console.log('hello world');
+
+const message = {
+    muscleGroup : ['upper', 'lower', 'core'],
+    upper : ['arnold press', 'bench press', 'pullover', 'rear-delt fly', 'bicep curl', 'chest fly'],
+    lower : ['static lunge', 'hip bridge', 'romanian deadlift', 'sumo squat', 'pistol squat', 'donkey kick'],
+    core : ['forearm plank', 'russian twist', 'buttterfly sit-up', 'dead bug', 'body saw', 'jack knife'],
+    sets : [1, 2, 3, 4, 5],
+    reps: [8 , 9, 10, 11, 12, 13, 14, 15, 16, 17]
+};
+
+
